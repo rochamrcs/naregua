2c6f83cd6a45056a86a73beda3b1f2b90805764d
rochamrcs <mavinicius95@gmail.com>
2024-08-10 12:08:00 -0300

    Layout da tela inicial 100% ajustada

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,46 @@
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import images from '../constants/images'
+import CustomButton from 'components/CustomButton';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-neutral-950">
-      <Text className="font-popBold text-3xl text-slate-50">NaRégua</Text>
-      <Link href={"./home"} className='mt-5 text-slate-200'>Vá para Perfil</Link>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+      contentContainerStyle={{
+        height: "100%",
+      }}
+      >
+
+        <View className="w-full flex justify-center items-center min-h-[85vh] space-y-3">
+
+          <Image
+            source={images.logo}
+            className='w-[142px] h-[142px]'
+            resizeMode='contain'
+          />
+          <Text className='text-slate-50 font-popBold text-3xl'>Na régua</Text>
+
+          <Text className='text-lg text-slate-50 mt-10 me-8 ms-8 text-center font-popRegular pt-14'>
+            Fique sempre na régua, agendamentos sem complicações !
+          </Text>
+
+        <CustomButton 
+          title="Entrar com email"
+          handlePress={() => router.push('/login')}
+          containerStyles="w-11/12 mt-7"
+        />
+
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor='#1E1C1F' style='light'>
+        
+      </StatusBar>
+      
+    </SafeAreaView>
   );
 }
