ce147998f57c7d83395a5fd05002c6a0c85d5dce
rochamrcs <mavinicius95@gmail.com>
2024-08-17 22:33:03 -0300

    Ajuste no nome do botão de acesso

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Redirect, router } from 'expo-router';
+import { router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -8,39 +8,39 @@ import CustomButton from 'components/CustomButton';
 
 export default function App() {
   return (
-    <SafeAreaView className="bg-primary h-full">
-      <ScrollView
-      contentContainerStyle={{
-        height: "100%",
-      }}
-      >
+      <SafeAreaView className="bg-primary h-full">
+        <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+        >
 
-        <View className="w-full flex justify-center items-center min-h-[85vh] space-y-3">
+          <View className="w-full flex justify-center items-center min-h-[85vh] space-y-3">
 
-          <Image
-            source={images.logo}
-            className='w-[142px] h-[142px]'
-            resizeMode='contain'
+            <Image
+              source={images.logo}
+              className='w-[142px] h-[142px]'
+              resizeMode='contain'
+            />
+            <Text className='text-slate-50 font-popBold text-3xl'>Na régua</Text>
+
+            <Text className='text-lg text-slate-50 mt-10 me-8 ms-8 text-center font-popRegular pt-14'>
+              Fique sempre na régua, agendamentos sem complicações !
+            </Text>
+
+          <CustomButton 
+            title="Entrar com usuário"
+            handlePress={() => router.push('/login')}
+            containerStyles="w-11/12 mt-7"
           />
-          <Text className='text-slate-50 font-popBold text-3xl'>Na régua</Text>
 
-          <Text className='text-lg text-slate-50 mt-10 me-8 ms-8 text-center font-popRegular pt-14'>
-            Fique sempre na régua, agendamentos sem complicações !
-          </Text>
+          </View>
+        </ScrollView>
 
-        <CustomButton 
-          title="Entrar com email"
-          handlePress={() => router.push('/login')}
-          containerStyles="w-11/12 mt-7"
-        />
-
-        </View>
-      </ScrollView>
-
-      <StatusBar backgroundColor='#1E1C1F' style='light'>
+        <StatusBar backgroundColor='#1E1C1F' style='light'>
+          
+        </StatusBar>
         
-      </StatusBar>
-      
-    </SafeAreaView>
+      </SafeAreaView>
   );
 }
